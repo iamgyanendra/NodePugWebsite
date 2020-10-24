4bf81fb03d9e4ae062d9a7d029dbeb36dd3aa8a7
Gyanendra Prakash <prakash9.2020@gmail.com>
2020-10-24 22:50:02 +0530

    Nodejs and mongodb

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); // body parser module for post throuh express 
+
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
 
 const port = 80;
+
+// define mongoose schema
+const contactSchema = new mongoose.Schema({
+
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String,
+  });
+
+  const Contact = mongoose.model('Contact', contactSchema);//model
 
 const app= express();
 
@@ -26,6 +42,18 @@ app.get('/contact', (req, res)=>{
 
     const params={}
     res.status(200).render('contact.pug',params);
+})
+app.post('/contact', (req, res)=>{
+
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+
+        res.send("Data is saved into the database")
+    }).catch(()=>{
+        res.status(400).send("Data is not saved in the database")
+
+    })
+   // res.status(200).render('contact.pug',params);
 })
 
 
